f3350fd1200799a1176703c321690aae9aa5f69d
mysoo <myungsoocc@gmail.com>
2024-08-23 09:24:21 +0900

    feat: entry/output

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+};
